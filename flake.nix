3da827d42d9eb6cb143d053b367033cfc6b337a2
Samuel Joli <samuel.joli.ftn@gmail.com>
2024-12-08 18:55:13 -0800

    wip

--- flake.nix
+++ flake.nix
@@ -1,128 +1,44 @@
 {
-  description = "Flake for baouncer, a conventional commit cli tool";
+  description = "Flake for baouncer cli";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
 
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nil.url = "github:oxalica/nil/c8e8ce72442a164d89d3fdeaae0bcc405f8c015a";
 
-  outputs = { self, nixpkgs, crane, fenix, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  inputs.nil.flake = true;
+
+  outputs =
+    {
+      nil,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        inherit (pkgs) lib;
-
-        craneLib = crane.mkLib pkgs;
-        src = craneLib.cleanCargoSource ./.;
-
-        # Common arguments can be set here to avoid repeating them later
-        commonArgs = {
-          inherit src;
-          strictDeps = true;
-
-          buildInputs = [
-            # Add additional build inputs here
-          ] ++ lib.optionals pkgs.stdenv.isDarwin [
-            # Additional darwin specific inputs can be set here
-            pkgs.libiconv
-          ];
-
-          # Additional environment variables can be set directly
-          # MY_CUSTOM_VAR = "some value";
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
         };
-
-        craneLibLLvmTools = craneLib.overrideToolchain
-          (fenix.packages.${system}.complete.withComponents [
-            "cargo"
-            "llvm-tools"
-            "rustc"
-          ]);
-
-        # Build *just* the cargo dependencies, so we can reuse
-        # all of that work (e.g. via cachix) when running in CI
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-
-        # Build the actual crate itself, reusing the dependency
-        # artifacts from above.
-        baouncer = craneLib.buildPackage (commonArgs // {
-          inherit cargoArtifacts;
-        });
+        nix-lsp-server = nil.packages.${system}.nil;
       in
       {
-        checks = {
-          # Build the crate as part of `nix flake check` for convenience
-          inherit baouncer;
-
-          # Run clippy (and deny all warnings) on the crate source,
-          # again, reusing the dependency artifacts from above.
-          #
-          # Note that this is done as a separate derivation so that
-          # we can block the CI if there are issues here, but not
-          # prevent downstream consumers from building our crate by itself.
-          baouncer-clippy = craneLib.cargoClippy (commonArgs // {
-            inherit cargoArtifacts;
-            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-          });
-
-          baouncer-doc = craneLib.cargoDoc (commonArgs // {
-            inherit cargoArtifacts;
-          });
-
-          # Check formatting
-          baouncer-fmt = craneLib.cargoFmt {
-            inherit src;
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              nix-lsp-server
+              rust-analyzer
+              openssl
+              rust-bin.beta.latest.default
+            ];
           };
-
-          # Run tests with cargo-nextest
-          # Consider setting `doCheck = false` on `baouncer` if you do not want
-          # the tests to run twice
-          baouncer-nextest = craneLib.cargoNextest (commonArgs // {
-            inherit cargoArtifacts;
-            partitions = 1;
-            partitionType = "count";
-          });
-        };
-
-        packages = {
-          default = baouncer;
-        } // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
-          baouncer-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs // {
-            inherit cargoArtifacts;
-          });
-        };
-
-        apps.default = flake-utils.lib.mkApp {
-          drv = baouncer;
-        };
-
-        devShells.default = craneLib.devShell {
-          # Inherit inputs from checks.
-          checks = self.checks.${system};
-
-          # Additional dev-shell environment variables can be set directly
-          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
-
-          # Extra inputs can be added here; cargo and rustc are provided by default.
-          packages = [
-            baouncer
-            fenix.packages.${system}.rust-analyzer
-          ];
-          env = {
-            # Required by rust-analyzer
-            RUST_SRC_PATH = "${fenix.packages.${system}.stable.rust-src}/lib/rustlib/src/rust/library";
-          };
-        };
-      });
+      }
+    );
 }
